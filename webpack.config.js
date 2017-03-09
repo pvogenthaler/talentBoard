586813b09ded9c22a44f1374355efc9fc2584235
Paige <paige.vogenthaler@gmail.com>
2017-03-08 21:14:29 -0800

    Create package.json, configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'app');
+var APP_DIR = path.resolve(__dirname, 'app');
+
+var config = {
+  entry: APP_DIR + '/containers/dashboard.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  externals: ['axios'],
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;
